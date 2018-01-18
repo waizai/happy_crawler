f9cd3dc4961718e20b20f37b95a3f1e6039d6002
dangxiaoli <dangxiaoli@meituan.com>
2018-01-18 19:08:09 +0800

    add

--- src/screenshot.js
+++ src/screenshot.js
@@ -2,19 +2,54 @@
 * @Author: dangxiaoli
 * @Date:   2018-01-18 15:32:42
 * @Last Modified by:   dangxiaoli
-* @Last Modified time: 2018-01-18 16:26:43
+* @Last Modified time: 2018-01-18 18:05:23
 */
+
+//任务
+//在百度搜索图片，爬取
+//分析
+//页面是懒加载，异步加载，用cheerio拿到的html几乎是空的；所以要用Headless Chorme模拟用户真实的访问;
+
 const puppeteer = require('puppeteer');
 const { screenshot } = require('./config/default.js');
 
 (async() => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    await page.goto('https://image.baidu.com');
+    console.log('go to https://image.baidu.com');
+    /*
+    *   如何拿到懒加载网页的内容
+    *   1. 不断滚动浏览器，不断触发懒加载
+    *   2. 给浏览器的窗口改的非常大，一次性加载完所有
+    */
 
-    await page.goto('https://www.baidu.com');
-    await page.screenshot({
-        path: `${screenshot}/${Date.now()}.png`
+    await page.evaluate({
+        width: 1920,
+        height: 1080
     });
+    console.log('reset viewPort');
+
+    /*
+     *  页面焦点到输入框
+     */
+    await page.focus('#kw');
+    await page.keyboard.sendCharacter('狗');
+
+    /*
+     *  触发按钮的点击
+     */
+    await page.click('.s_btn');
+    console.log('go to a search list');
+
+    /*
+     *  等待加载完成
+     */
+     page.on('load', () => {
+        console.log('page loading done, start fetch...');
+     })
 
     await browser.close();
 })();
+
+
